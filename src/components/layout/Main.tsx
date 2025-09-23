e950d92c7eeb26c6fe9db225b74a106a8e925894
mikemem44 <mikelmm1999@gmail.com>
2025-09-23 09:49:44 -0600

    feat: ProductCarousel fue re-escrito utilizando Embla Carousel, en lugar del componente Carousel de ShadCn
    * Agregados subcomponentes ProductCarouselAutoplay, ProductCarouselArrowButtons, ProductCarouselDotButton
    
    * Variante responsive fue diseñada para mobiles
    
    * Clases CSS para embla fueron agregadas a global.css

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,13 +1,18 @@
 import { FC, ReactNode } from "react";
-import ProductCarousel from "./main/ProductCarousel";
+import ProductCarousel from './main/ProductCarousel'
+import { EmblaOptionsType } from 'embla-carousel'
 
 type MainProps = {
   children: ReactNode;
 };
 
+const OPTIONS: EmblaOptionsType = { loop: true, duration: 30 }
+const SLIDE_COUNT = 3
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
+
 const Main: FC<Readonly<MainProps>> = ({ children }) => {
   return <main className="flex-1 flex flex-col items-center">
-    <ProductCarousel />
+    <ProductCarousel slides={SLIDES} options={OPTIONS} />
     { children }
   </main>;
 };
--- src/components/layout/main/ProductCarousel.tsx
+++ src/components/layout/main/ProductCarousel.tsx
@@ -1,18 +1,31 @@
-import Image, { StaticImageData } from "next/image";
+"use client"
+
+import React, { useCallback } from 'react'
+import { EmblaOptionsType, EmblaCarouselType } from 'embla-carousel'
+import useEmblaCarousel from 'embla-carousel-react'
+import Fade from 'embla-carousel-fade'
+import {
+  NextButton,
+  PrevButton,
+  usePrevNextButtons
+} from './ProductCarouselArrowButtons'
+import Autoplay from 'embla-carousel-autoplay'
+import { useAutoplay } from './ProductCarouselAutoplay'
+import { DotButton, useDotButton } from './ProductCarouselDotButton'
+import Image, { StaticImageData } from 'next/image'
 import promoSwitchMobile from "@/assets/images/promoSwitchMobile.webp";
 import promoPS5Mobile from "@/assets/images/promoPS5Mobile.webp";
 import promoSwitch from "@/assets/images/promoSwitch.webp";
 import promoPS5 from "@/assets/images/promoPS5.webp";
 import promoXbox from "@/assets/images/promoXbox.webp";
 import promoXboxMobile from "@/assets/images/promoXboxMobile.webp";
-import { Card, CardContent } from "@/components/ui/card";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel"
+import { Play, Pause } from 'lucide-react'
+
+
+type PropType = {
+  slides: number[]
+  options?: EmblaOptionsType
+}
 
 type CarouselItemType = {
   label: string;
@@ -38,53 +51,80 @@ export const productCarouselItem: CarouselItemType[] = [
   },
 ];
 
-const ProductCarousel = () => {
-    return (
-    <Carousel 
-         opts={{
-            align: "start",
-            loop: true,
-            skipSnaps: true,
-        }}
-        className="w-full max-w-sm md:max-w-11/12 relative z-0">
-        {/* //PEDI AYUDA A LA IA YA POR ULTIMO PASA A LO SIGUIENTE Y PEDI AYUDA A HENRY
-        //RE: PERSEVERAR EL ROUND EDGE DEL BORDER RADIUS */}
-      <CarouselContent>
-        {productCarouselItem.map((item,index) => (
-          <CarouselItem key={index}>
-            <div className="relative">  
-              <Card className="rounded-2xl">
-                <CardContent className="flex items-center justify-center h-90 md:h-140 p-6 ">
-                  <div className="md:hidden">
-                    <Image 
-                      src={item.pathMobile}
-                      alt={item.label}
-                      fill
-                      className="object-cover"
-                      sizes="(max-width: 767px) 100vw, 0vw"
-                    />
-                  </div>
-                  <div className="hidden md:block">
-                    <Image 
-                      src={item.path}
-                      alt={item.label}
-                      fill
-                      className="object-cover block rounded-2xl"
-                      sizes="(min-width: 767px) 100vw, 0vw"
-                    />
-                  </div>
-                  
-                </CardContent> 
-              </Card>
+const ProductCarousel: React.FC<PropType> = (props) => {
+  const { options } = props
+  const [emblaRef, emblaApi] = useEmblaCarousel(options, [Fade(), Autoplay({ playOnInit: false, delay: 10000 })])
+
+  const onNavButtonClick = useCallback((emblaApi: EmblaCarouselType) => {
+    const autoplay = emblaApi?.plugins()?.autoplay
+    if (!autoplay) return
+
+    const resetOrStop =
+      autoplay.options.stopOnInteraction === false
+        ? autoplay.reset
+        : autoplay.stop
+
+    resetOrStop()
+  }, [])
+
+  const { selectedIndex, scrollSnaps, onDotButtonClick } =
+    useDotButton(emblaApi)
+
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick
+  } = usePrevNextButtons(emblaApi, onNavButtonClick)
+
+  const { autoplayIsPlaying, toggleAutoplay, onAutoplayButtonClick } =
+    useAutoplay(emblaApi)
+
+  return (
+    <div className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {productCarouselItem.map((item,index) => (
+            <div className="embla__slide" key={index}>
+              <Image 
+                src={item.pathMobile}
+                alt={item.label}
+                className="embla__slide__img md:hidden"
+              />
+              <Image 
+                src={item.path}
+                alt={item.label}
+                className="embla__slide__img hidden md:block"
+              />
             </div>
-          </CarouselItem>
-        ))}
-        
-      </CarouselContent>
-      <CarouselPrevious className="absolute left-6 bg-white" />
-      <CarouselNext className="absolute right-6 bg-white" />
-    </Carousel>
-    )
+          ))}
+        </div>
+      </div>
+
+      <div className="embla__controls hidden md:block">
+        <div className="embla__buttons">
+          <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+        </div>
+          
+        <div className="embla__dots">
+          {scrollSnaps.map((_, index) => (
+            <DotButton
+              key={index}
+              onClick={() => onDotButtonClick(index)}
+              className={'embla__dot'.concat(
+                index === selectedIndex ? ' embla__dot--selected' : ''
+              )}
+            />
+          ))}
+        </div>
+
+        <button className="embla__play" onClick={toggleAutoplay} type="button">
+          {autoplayIsPlaying ? <Pause className='stroke-[#5cd49c]'/> : <Play className='stroke-[#5cd49c]'/>}
+        </button>
+      </div>
+    </div>
+  )
 }
 
-export default ProductCarousel;
+export default ProductCarousel
